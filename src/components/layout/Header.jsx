36003e2216fbc0ea7be52e6a1866965a4aa90984
Todd Anderson <todd@thehomie.com>
2017-06-15 11:47:09 -0700

    start readme, bug fix, header work, dec 18

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,21 +3,39 @@ import { connect } from 'react-redux';
 import store from '../../store/store';
 import { keyGenerator } from '../../util/keygenerator';
 import { prefix } from '../../util/prefix';
+import { emptyFn } from '../../util/emptyFn';
 import '../../style/components/header.styl';
 import { CLASS_NAMES } from '../../constants/GridConstants';
 
 class Header extends Component {
 
-    // define what the properties passed into this class should look like
     static defaultProps = {
         columns: React.PropTypes.arrayOf(React.PropTypes.Object).isRequired
     }
 
-    // P column name and val are passed into the generator to make one string nameval?
     getHeader(col) {
+
+        let headerProps = {
+            className: col.className,
+            onClick: col.HANDLE_CLICK || emptyFn
+        };
+
+        if (col.width) {
+            headerProps = Object.assign(
+                headerProps, { style: { width: col.width } });
+        }
+
+        const innerHTML = col.renderer ? col.renderer(col) : col.name;
+
         return (
-            <th key={keyGenerator(col.name, col.value) }>{ col.name }</th>
+            <th { ...headerProps } key={keyGenerator(col.name, col.value) }>
+                { innerHTML }
+            </th>
         );
+    }
+
+    getAdditionalClasses() {
+
     }
 
     render() {
@@ -29,18 +47,13 @@ class Header extends Component {
         }
 
         return (
-            <tr {...headerProps}>
+            <tr { ...headerProps }>
                 { headers }
             </tr>
         );
     }
 }
 
-// I see this function is listed here and in the parent file, Grid.jsx.
-// I imagine this is a way to make this functionality available in each file that needs it.
-// So, the application state is accessible from here just as it is from Grid.jsx
-// P What then, is the significance of passing const HeaderProps = { columns: columns }; into this file?
-// R this.props has only columns defined in the state, here!
 function mapStateToProps() {
     return {};
 }
