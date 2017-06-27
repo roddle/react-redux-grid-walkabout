10a211133df70f87086b474f5691e794d1e40fe1
Todd Anderson <todd@thehomie.com>
2017-06-27 16:28:12 -0700

    remote n local paging most done dec 20

--- src/actions/GridActions.js
+++ src/actions/GridActions.js
@@ -1,24 +1,45 @@
 import {
-    GET_DATA,
-    SET_DATA
-} from '../constants/ActionTypes';
+    PAGE_LOCAL,
+    PAGE_REMOTE,
+    SET_DATA,
+    ERROR_OCCURRED
+} from '../../../constants/ActionTypes';
 
-import Request from '../components/plugins/ajax/Request';
+import Request from '../../../components/plugins/ajax/Request';
 
-export function getAsyncData(datasource) {
+export function setPage(index, type, BUTTON_TYPES) {
+
+    const pageIndex = type === BUTTON_TYPES.NEXT ? index + 1 : index - 1;
+
+    return { type: PAGE_LOCAL, pageIndex };
+}
+
+export function setPageAsync(index, pageSize, type, BUTTON_TYPES, datasource) {
+
+    const pageIndex = type === BUTTON_TYPES.NEXT ? index + 1 : index - 1;
 
     return (dispatch) => {
 
         return Request.api({
             route: datasource,
-            method: 'GET'
+            method: 'POST',
+            data: {
+                pageIndex: pageIndex,
+                pageSize: pageSize
+            }
         }).then((response) => {
 
             if (response) {
 
                 dispatch({
+                    type: PAGE_REMOTE,
+                    pageIndex: pageIndex
+                });
+
+                dispatch({
                     type: SET_DATA,
-                    data: response,
+                    data: response.data,
+                    total: response.total,
                     success: true
                 });
 
@@ -26,17 +47,11 @@ export function getAsyncData(datasource) {
 
             else {
                 dispatch({
-                    type: SET_DATA,
-                    data: [],
-                    success: false
+                    type: ERROR_OCCURRED,
+                    error: response
                 });
             }
 
         });
-
     };
-}
-
-export function setData(data) {
-    return { type: SET_DATA, data };
 }
